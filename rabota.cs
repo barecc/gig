b569a536d8f47dfc6b54a94645b684f108fe96d3
David Barinov <davidbarinov@MacBook-Pro-David.local>
2023-01-25 22:06:30 +0300

    Step6

--- rabota.cs
+++ rabota.cs
@@ -1,1 +1,12 @@
 Console.Clear();
+string[] array1 = {"123", "23", "da", "net", "hz"};
+string[] array2 = array1.Where(x => x.Length <= 3).ToArray();
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array2);
